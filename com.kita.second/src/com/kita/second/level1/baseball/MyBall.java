c4b08efc9e297eb098d1e1df9d7dc145a874064e
hwan789 <younghwankim0102@naver.com>
2021-02-28 17:49:49 +0900

    02.28.sun

--- com.kita.second/src/com/kita/second/level1/baseball/MyBall.java
+++ com.kita.second/src/com/kita/second/level1/baseball/MyBall.java
@@ -1,5 +1,28 @@
 package com.kita.second.level1.baseball;
 
-public class MyBall {
+import java.util.Scanner;
 
+public class MyBall {
+	// 내가 숫자값 입력하고 예외 처리하는 클래스
+		// 필드
+		private int[] myArr;
+		private Scanner scan;
+
+		public MyBall(int gameCnt) {
+			myArr = new int[gameCnt];
+			scan = new Scanner(System.in);
+		}
+
+		// 메소드
+		// 숫자를 입력받기 setNumbers()
+		// 내 배열안의 숫자값 뽑기 getMyNum()
+		void setNumbers() {
+			for() {//myArr의 길이만큼 scanner사용해서 값 입력받기
+				
+			}
+		}
+		
+		int getMyNum(int idx) {
+			return myArr[idx];
+		}
 }
